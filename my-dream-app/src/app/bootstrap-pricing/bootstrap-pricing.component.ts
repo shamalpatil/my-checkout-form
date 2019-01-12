099cbdbcfed6b5a0c6281105958afa7b964deb43
shamalpatil <patilshamal93@gmail.com>
2019-01-12 19:48:41 +0530

    simple checkout form

---
+++ my-dream-app/src/app/bootstrap-pricing/bootstrap-pricing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-pricing',
+  templateUrl: './bootstrap-pricing.component.html',
+  styleUrls: ['./bootstrap-pricing.component.css']
+})
+export class BootstrapPricingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
